3ab9d6e1bdf0bdb92f37fe82787dc882cf50f783
jchurley95 <jchurley95@gmail.com>
2017-07-25 00:07:10 -0400

    Shakespeare out

--- exercises/to_reject_or_not_to_reject/exercise.js
+++ exercises/to_reject_or_not_to_reject/exercise.js
@@ -66,4 +66,15 @@ exercise.addVerifyProcessor(function (callback) {
   });
 });
 
+var promise = new Promise((fulfill, reject) => {
+  fulfill('I FIRED');
+  reject(new Error('I DID NOT FIRE'));
+});
+
+function onReject(error) {
+  console.log(error.message);
+}
+
+promise.then(console.log, onReject);
+
 module.exports = exercise;
