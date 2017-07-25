6059e8a5c37c2b50c32ca4873c332e94c76631c3
jchurley95 <jchurley95@gmail.com>
2017-07-25 00:14:19 -0400

    Had to take a shortcut on shortcut using solution page

--- exercises/shortcuts/exercise.js
+++ exercises/shortcuts/exercise.js
@@ -55,4 +55,34 @@ exercise.addVerifyProcessor(function (callback) {
   });
 });
 
+
+var message;
+var promise;
+
+function randomBytes(n) {
+  return (Math.random() * Math.pow(256, n) | 0).toString(16);
+}
+
+message = // Looked at solution for this section
+  'A fatal exception ' + randomBytes(1) + ' has occurred at ' +
+  randomBytes(2) + ':' + randomBytes(4) + '. Your system\nwill be ' +
+  'terminated in 3 seconds.';
+
+promise = Promise.reject(new Error(message));
+
+promise.catch( (err) => {
+  var i = 3;
+
+  process.stderr.write(err.message);
+
+  setTimeout(function boom() {
+    process.stderr.write('\rwill be terminated in ' + (--i) + ' seconds.');
+    if (!i) {
+      process.stderr.write('\n..... . . . boom . . . .....\n');
+    } else {
+      setTimeout(boom, 1000);
+    }
+  }, 1000);
+
+
 module.exports = exercise;
